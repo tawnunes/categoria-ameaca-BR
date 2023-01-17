e409f039bb915ebb9d57dc36d1a0d87d5eaebc8a
Tawane Nunes <taw.ynunes@gmail.com>
2023-01-17 11:04:35 -0300

    Adicionado especificação de páginas contendo as tabelas a serem extraídas do pdf

--- R/extrair_tabela_pdf.R
+++ R/extrair_tabela_pdf.R
@@ -11,14 +11,15 @@
 
 # PACOTES ----
 
-library(remotes) # para ser possível baixar o pacote tabulizer a partir do repositório no github
+# library(remotes) # para ser possível baixar o pacote tabulizer a partir do repositório no github
 # intalação do pacote
-remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch") 
+# remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch") 
 
 library(tabulizer) # pacote para extração de dados do pdf
 # para utilização do tabulizer é necessário o download do java via https://www.java.com/en/download/
 
-library(tidyverse)
+library(tidyverse) # instala outros pacores do R, principalmente para 
+# Manipulação de dados
 
 # CHECAR SE OS PASSOS SEGUINTES SÃO NECESSÁRIOS ----
 library(rJava) # necessário para utilização do tabulizer
@@ -30,11 +31,21 @@ library(remotes)
 remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch") 
 
 
-# EXTRAIR TABELA DO PDF
 
-tabela_especies <- extract_tables("./docs/lista_especies_ameacadas.pdf",
-                                  output = "data.frame") 
+# EXTRAIR TABELA DO PDF ----
 
+# criação de objeto com as páginas para facilitar entrada para usuário
+paginas <- c(2:5) 
+
+# extração das tabelas
+tabela_especies <- extract_tables("./docs/lista_especies_ameacadas.pdf", 
+                                  pages = paginas, # especificação das páginas
+                                  output = "data.frame") # formato do resultado
+
+# Obs.: O arquivo criado é uma lista o que significa que cada tabela é um item.
+
+
+# TRANSFORMAR LISTA EM DATA FRAME ----
 teste_extracao <- tabela_especies[[1]]
 
 if (!dir.exists("dados/processados")) dir.create("dados/processados")
