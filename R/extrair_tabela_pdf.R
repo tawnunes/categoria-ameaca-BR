004f77009d1a9f8749bd4e0a473e1eff8d0172af
Tawane Nunes <taw.ynunes@gmail.com>
2023-01-23 10:58:12 -0300

    Tendo problemas com automatização da padronização das tabelas visto que o pdf tem muitas particularidades

--- R/extrair_tabela_pdf.R
+++ R/extrair_tabela_pdf.R
@@ -5,13 +5,14 @@
 # 
 # Autora: Tawane Yara Nunes
 # E-mail: taw.ynunes@gmail.com
-# 
+# -------------------------------------------------------------------------------
 
 # EXTRAIR DADOS DE TABELAS CONTIDAS EM ARQUIVO PDF
 
 # PACOTES ----
 
-# library(remotes) # para ser possível baixar o pacote tabulizer a partir do repositório no github
+# library(remotes) # para ser possível baixar o pacote tabulizer a partir do repositório 
+# no github
 # intalação do pacote
 # remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch") 
 
@@ -22,20 +23,16 @@ library(tidyverse) # instala outros pacores do R, principalmente para
 # Manipulação de dados
 
 # CHECAR SE OS PASSOS SEGUINTES SÃO NECESSÁRIOS ----
-library(rJava) # necessário para utilização do tabulizer
+#library(rJava) # necessário para utilização do tabulizer
 
 # Criando um ambiente java 
-Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk-18/")
-
-library(remotes)
-remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch") 
-
+# Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk-18/")
 
 
 # EXTRAIR TABELA DO PDF ----
 
 # criação de objeto com as páginas para facilitar entrada para usuário
-paginas <- c(2:5) 
+paginas <- c(98:101) 
 
 # extração das tabelas
 tabela_especies <- extract_tables("./docs/lista_especies_ameacadas.pdf", 
@@ -43,26 +40,25 @@ tabela_especies <- extract_tables("./docs/lista_especies_ameacadas.pdf",
                                   output = "data.frame") # formato do resultado
 
 # EXCLUIR DATA FRAMES DESNECESSÁRIOS -----
-
 # visualização
 lapply(tabela_especies, View)
 
 # Excluir
 
-# tabela_especies <- tabela_especies[-1] # Descomentar se necessário
+tabela_especies <- tabela_especies[-1] # Descomentar se necessário
 
-# conferindo se é o dataframe com o nome das colunas 
-View(tabela_especies[[1]])
+# conferindo se é o dataframe com o nome das colunas
 
 # TRANSFORMAR LISTA EM DATA FRAME ----
 
 # RENOMEANDO COLUNAS
+nome_colunas <- c("ID","lista anterior","familia", "espécie ou subspecie","categoria")
 
 # Capturando o nome das colunas contidos na primera parte da tabela
 nome_colunas <- colnames(tabela_especies[[1]])
 # Aplicando o mesmo nome de coluna em todas os data frames da lista
 to_df <- lapply(tabela_especies, setNames, nome_colunas)
-
+lapply(to_df, View)
 
 
 # TRANFORMANDO O RESULTADO EM UM DATA FRAME
