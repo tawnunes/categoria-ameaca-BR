cff9bddd0fcabf73b4aaa8083258f3386d2e1454
Tawane Nunes <taw.ynunes@gmail.com>
2023-01-30 16:12:11 -0300

    Alterando dados para unir fauna e flora em um unico dataframe

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -23,17 +23,25 @@ df_fauna <- read.csv("./dados/raw/lista_fauna_raw.csv")
 ## FLORA ------------------------------------------------------------------------------------
 
 df_flora <- df_flora %>% mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
-                                lista_anterior = ifelse(lista_anterior == "*", "sim", "nao")) %>%  # alterando marcação de listagem anterior
-                         filter(!is.na(ID)) # excluindo linhas que não contém ID númerico
+                                lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                                grupo = "Flora") %>% # marcando o grupo
+                         filter(!is.na(ID))  # excluindo linhas que não contém ID númerico
 # Estas linhas são geradas por cabeçalhos e outras informações na tabela
 
 ## FAUNA --------------------------------------------------------------------------------------
 
 
 df_fauna <- df_fauna %>% mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
-                                    lista_anterior = ifelse(lista_anterior == "*", "sim", "nao")) %>%  # alterando marcação de listagem anterior
+                                    lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                                grupo = "Fauna") %>%  # marcando o grupo 
                              filter(!is.na(ID)) # excluindo linhas que não contém ID númerico
 # Estas linhas são geradas por cabeçalhos e outras informações na tabela
+
+# UNINDO OS DOIS GRUPOS ---------------------------------------------------------------------
+
+lista_ameaca_BR <- df_fauna %>% bind_rows(df_flora) %>% select(c(grupo, especie, familia, ordem, categoria, lista_anterior)) 
+
+
 
 # EXPORTANDO OS DADOS GERADOS ---------------------------------------------------------------
 
@@ -45,4 +53,7 @@ write.csv(df_flora, "dados/processados/lista_flora.csv",
           row.names = FALSE)
 
 write.csv(df_fauna, "dados/processados/lista_fauna.csv", 
+          row.names = FALSE)
+
+write.csv(lista_ameaca_BR, "dados/processados/lista_especies_ameacadas.csv", 
           row.names = FALSE)
