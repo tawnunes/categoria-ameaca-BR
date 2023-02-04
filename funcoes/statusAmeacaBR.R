8d9194a385144fa9a1fb2d9d3d40f0a8ac86835e
Tawane Nunes <taw.ynunes@gmail.com>
2023-02-04 12:36:39 -0300

    Renomear e atulizar função, incluindo pacote necessário e formatação do csv de saída

--- funcoes/statusAmeacaBR.R
+++ funcoes/statusAmeacaBR.R
@@ -10,18 +10,37 @@
 # CONFERIR STATUS DE AMEAÇA
 
 statusAmeacaBR <- function(lista){
+ # carregar pacotes necessários----
+ 
+ # Carregamento de todos os pacotes necessários a execução deste projeto
+ 
+ pacotes <- c("tidyverse") # instala outros pacores do R, principalmente  
+ 
+ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for(i in 1:length(instalador)) {
+   install.packages(instalador, dependencies = T)
+   break()}
+  sapply(pacotes, require, character = T) 
+ } else {
+  sapply(pacotes, require, character = T)}
+ 
+ rm(pacotes)
+ 
+ # aplicando a função -----
  
  lista <- read.csv2(lista)
  
- lista_ameacadas <- read.csv("./dados/processados/lista_especies_ameacadas.csv")
+ lista_ameacadas <- read.csv("./dados/processados/lista_especies_ameacadas_P148.csv")
  
- lista_ameacadas <- lista_ameacadas %>% select(c(especie, categoria)) %>% 
-                                        rename(status_portaria148 = categoria)
+ lista_ameacadas <- lista_ameacadas %>% select(c(especie, categoria_portaria148))
  
- nova_lista <- lista %>% left_join(lista_ameacadas, by = "especie")
+ nova_lista <- lista %>% left_join(lista_ameacadas, by = "especie") %>% 
+  mutate(categoria_portaria148 = replace_na(categoria_portaria148, "nao listado")) %>% 
+  relocate(categoria_portaria148, .after = especie)
  
  
  write_excel_csv2(nova_lista, "resultado_status_ameaca.csv")
  
- print("A lista conferida encontra-se no seu diretorio")
+ print("Pronto!")
 }
