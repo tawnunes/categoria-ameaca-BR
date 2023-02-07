63181ab0135efdbe0c3e108013c6babb8ead369b
Tawane Nunes <taw.ynunes@gmail.com>
2023-02-07 09:30:41 -0300

    Corrigindo erro na função filter, não consegui usar o operador lógico |, inclui multiplos filter para funcionar

--- R/02_Portaria148_data_cleaning.R
+++ R/02_Portaria148_data_cleaning.R
@@ -25,52 +25,63 @@ fauna_aquatica_P148 <- read.csv("./dados/raw/lista_fauna_aquatica_raw.csv")
 
 ## FLORA ------------------------------------------------------------------------------------
 
-flora_P148 <- flora_P148 %>% mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
-                                lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
-                                componente = "Flora") %>% # marcando o componente
-                         filter(!is.na(ID))  # excluindo linhas que não contém ID númerico
-# Estas linhas são geradas por cabeçalhos e outras informações na tabela
+flora_P148 <- flora_P148 %>% 
+              mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
+                     lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                     componente = "Flora") %>% # marcando o componente
+              filter(!is.na(ID))  # excluindo linhas que não contém ID númerico
+             # Estas linhas são geradas por cabeçalhos e outras informações na tabela
 
 ## FAUNA --------------------------------------------------------------------------------------
 
 ### FAUNA GERAL -----------------------------------------------------------------------------------------------------------
 
-fauna_geral_ameacada <- fauna_geral_P148 %>% filter(categoria != "EX"|categoria != "RE") %>% 
-                                                 mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
-                                                        lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
-                                                        componente = "Fauna", # marcando o componente
-                                                        grupo = ifelse(ID >= 1 & ID <= 275, "Invertebrados terrestres", 
-                                                                       ifelse(ID >= 276 & ID <= 334 , "Anfibios",
-                                                                              ifelse(ID >= 335 & ID <= 405, "Repteis",
-                                                                                     ifelse(ID >= 406 & ID <= 662, "Aves",
-                                                                                            ifelse(ID >= 663 & ID <= 764, "Mamiferos", 
-                                                                                                   NA)))))) %>%  # marcando os grupo
-                                                 filter(!is.na(ID)) # excluindo linhas que não contém ID númerico
-# Estas linhas são geradas por cabeçalhos e outras informações na tabela
+fauna_geral_ameacada <- fauna_geral_P148 %>% 
+                        mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
+                               lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                               componente = "Fauna", # marcando o componente
+                               grupo = ifelse(ID >= 1 & ID <= 275, "Invertebrados terrestres", 
+                                              ifelse(ID >= 276 & ID <= 334 , "Anfibios",
+                                                     ifelse(ID >= 335 & ID <= 405, "Repteis",
+                                                            ifelse(ID >= 406 & ID <= 662, "Aves",
+                                                                   ifelse(ID >= 663 & ID <= 764, "Mamiferos", 
+                                                                          NA)))))) %>%  # marcando os grupo
+                        filter(!is.na(ID) )  %>% # excluindo linhas que não contém ID númerico
+                        # Estas linhas são geradas por cabeçalhos e outras informações na tabela
+                        filter(categoria != "EX" ) %>%  # separando as espécies ja extintas para facilitar agrupamento
+                        filter(categoria != "RE") # por algum motivo não consegui fazer o filter usando | 
 
-fauna_geral_extinta <- fauna_geral_P148 %>% filter(categoria == "EX"|categoria == "RE") %>%
-                                     mutate(ID = as.numeric(ID),
-                                            grupo = ifelse(ordem == "Anura", "Anfibios",
-                                                    ifelse(ordem == "Charadriiformes" | ordem == "Passeriformes" |
-                                                            ordem == "Strigiformes"| ordem == "Psittaciformes", 
-                                                           "Aves", "Mamiferos")))
+
+#### FAUNA EXTINTA ----
+
+fauna_geral_extinta <- fauna_geral_P148 %>% 
+                       filter(categoria == "EX"|categoria == "RE") %>%
+                       mutate(ID = as.numeric(ID),
+                              lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                              componente = "Fauna", # marcando o componente
+                              grupo = ifelse(ordem == "Anura", "Anfibios",
+                                             ifelse(ordem == "Charadriiformes" | ordem == "Passeriformes" |
+                                                     ordem == "Strigiformes"| ordem == "Psittaciformes", 
+                                                    "Aves", "Mamiferos")))
 
 ### FAUNA AQUATICA ---------------------------------------------------------------------------------------------------
 
-fauna_aquatica_P148 <- fauna_aquatica_P148 %>% mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
-                                                        lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
-                                                        componente = "Fauna", # marcando o componente
-                                                        grupo = ifelse(ID >= 1 & ID <= 97, "Invertebrados aquaticos", 
-                                                                       ifelse(ID >= 98 & ID <= 485 , "Peixes", NA))) %>%  # marcando os grupo
-                                               filter(!is.na(ID)) # excluindo linhas que não contém ID númerico
-# Estas linhas são geradas por cabeçalhos e outras informações na tabela
+fauna_aquatica_P148 <- fauna_aquatica_P148 %>% 
+                       mutate(ID = as.numeric(ID), # transformando a coluna ID em numeric para facilitar a limpeza
+                              lista_anterior = ifelse(lista_anterior == "*", "sim", "nao"),# alterando marcação de listagem anterior
+                              componente = "Fauna", # marcando o componente
+                              grupo = ifelse(ID >= 1 & ID <= 97, "Invertebrados aquaticos", 
+                                             ifelse(ID >= 98 & ID <= 485 , "Peixes", NA))) %>%  # marcando os grupo
+                       filter(!is.na(ID)) # excluindo linhas que não contém ID númerico
+                      # Estas linhas são geradas por cabeçalhos e outras informações na tabela
 
 
 # UNINDO OS DOIS COMPONENTES ---------------------------------------------------------------------
 
-lista_ameaca_BR_P148 <- fauna_geral_ameacada %>% bind_rows(list(fauna_geral_extinta,fauna_aquatica_P148, flora_P148)) %>% 
-                                          select(c(componente,grupo,familia, ordem, especie, categoria, lista_anterior)) %>% 
-                                          rename(categoria_portaria148 = categoria)
+lista_ameaca_BR_P148 <- fauna_geral_ameacada %>%
+                        bind_rows(list(fauna_geral_extinta,fauna_aquatica_P148, flora_P148)) %>% 
+                        select(c(componente,grupo,familia, ordem, especie, categoria, lista_anterior)) %>% 
+                        rename(categoria_portaria148 = categoria)
 
 
 
